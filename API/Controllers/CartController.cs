bc363286458150f278a569510e55a44691cf0c00
Marijan Novak <zgmarijan@gmail.com>
2024-11-08 10:12:42 +0100

    End of section 12

---
+++ API/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+using System;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+public class CartController(ICartService cartService) : BaseAPIController
+{
+    [HttpGet]
+    public async Task<ActionResult<ShoppingCart>> GetCartById(string id)
+    {
+        var cart = await cartService.GetCartAsync(id);
+
+        return Ok(cart ?? new ShoppingCart{ Id = id });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCart>> UpdateCart(ShoppingCart cart)
+    {
+        var updatedCart = await cartService.SetCartAsync(cart);
+
+        if(updatedCart == null) return BadRequest("Problem with carts");
+
+        return updatedCart;
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteCart(string id)
+    {
+        var result = await cartService.DeleteCartAsync(id);
+
+        if(!result) return BadRequest("Problem deleting cart");
+
+        return Ok();
+    }
+}
