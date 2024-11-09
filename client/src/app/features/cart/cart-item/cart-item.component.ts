f2a57e428733d53488e7e8db3025623f68a5715c
Marijan Novak <zgmarijan@gmail.com>
2024-11-09 13:07:55 +0100

    End of section 13

---
+++ client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, input } from '@angular/core';
+import { CartItem } from '../../../shared/models/cart';
+import { RouterLink } from '@angular/router';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../../../core/services/cart.service';
+
+@Component({
+  selector: 'app-cart-item',
+  standalone: true,
+  imports: [RouterLink, MatButton, MatButtonModule, MatIcon, CurrencyPipe],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss',
+})
+export class CartItemComponent {
+  item = input.required<CartItem>();
+  cartService = inject(CartService);
+
+  incrementQuantity() {
+    this.cartService.addItemToCart(this.item());
+  }
+
+  decrementQuantity() {
+    this.cartService.removeItemFromCart(this.item().productId);
+  }
+
+  removeItemFromCart() {
+    this.cartService.removeItemFromCart(
+      this.item().productId,
+      this.item().quantity
+    );
+  }
+}
